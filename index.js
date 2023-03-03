f7ea07317cd99ddfc7c35d5df7d39ff44d1f3f0b
Tomas Peters <tdpeters6794@gmail.com>
2023-03-03 15:36:20 -0500

    trying to track wins. added variables for player and computer points. idea is that inside playGame function, the round is played, and then based on result of checkWinner, a playerPoint or computerPoint should increase by 1. But it's not increasing.

--- index.js
+++ index.js
@@ -2,19 +2,43 @@ const playerText = document.querySelector("#playerText")
 const computerText = document.querySelector("#computerText")
 const resultText = document.querySelector("#resultText")
 const choiceBtns = document.querySelectorAll(".choiceBtn")
+const playerScore = document.querySelector("#playerScore")
+const computerScore = document.querySelector("#computerScore")
+
 
 let player
 let computer
 let result
 
-choiceBtns.forEach(button => button.addEventListener("click", () => {
+playGame()
 
-    player = button.textContent
-    computerTurn();
-    playerText.textContent = `Player: ${player}`
-    computerText.textContent = `Computer: ${computer}`
-    resultText.textContent = checkWinner()
-}))
+function playGame(){
+    let playerPoint = 0
+    let computerPoint = 0
+
+    playRound()
+
+    if (resultText == "You Win!"){
+        playerPoint++
+    }
+    else if (resultText == "You Lose!"){
+        computerPoint++
+    }
+    
+    playerScore.textContent = `Player: ${playerPoint}`
+    computerScore.textContent = `Computer: ${computerPoint}`
+}
+
+function playRound(){
+
+    choiceBtns.forEach(button => button.addEventListener("click", () => {
+        player = button.textContent
+        computerTurn();
+        playerText.textContent = `Player: ${player}`
+        computerText.textContent = `Computer: ${computer}`
+        resultText.textContent = checkWinner()
+    }))
+    }
 
 
 function computerTurn(){
@@ -34,17 +58,17 @@ function computerTurn(){
     }
 }
 
-function checkWinner (){
+function checkWinner(){
     if(player == computer){
-        return "Draw!"
+      return "Draw!";
     }
-    else if (computer == "BOULDER"){
-        return (player == "PARCHMENT") ? "You Win!" : "You Lose!"
+    else if(computer == "BOULDER"){
+      return (player == "PARCHMENT") ? "You Win!" : "You Lose!"
     }
-    else if (computer == "PARCHMENT"){
-        return (player == "SHEARS") ? "You Win!" : "You Lose!"
+    else if(computer == "PARCHMENT"){
+      return (player == "SHEARS") ? "You Win!" : "You Lose!"
     }
-    else if (computer == "SHEARS"){
-        return (player == "BOULDER") ? "You Win!" : "You Lose!"
+    else if(computer == "SHEARS"){
+      return (player == "BOULDER") ? "You Win!" : "You Lose!"
     }
 }
